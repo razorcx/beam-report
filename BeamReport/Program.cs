5c60cc54972eaaa6ddf71739891fb20d6b859699
Cory LePage <cory@lepagegroup.ca>
2017-11-22 17:07:03 -0800

    Initial commit

--- BeamReport/Program.cs
+++ BeamReport/Program.cs
@@ -1,15 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using CsvHelper;
+using Tekla.Structures.Model;
 
 namespace BeamReport
 {
-	class Program
+	public class Program
 	{
 		static void Main(string[] args)
 		{
+			ModelObjectEnumerator.AutoFetch = true;
+			var modelObjectEnumerator =
+				new Model().GetModelObjectSelector().GetAllObjects();
+
+			var modelObjects = modelObjectEnumerator.ToList();
+
+			var beams = modelObjects.OfType<Beam>().ToList();
+
+			var beamsReport = beams.Select(b => new
+				{
+					b.Name,
+					b.Profile.ProfileString,
+					b.Class,
+					b.Finish,
+					b.Material.MaterialString
+				})
+				.ToList();
+
+			var modelName = new Model().GetInfo().ModelName.Split('.')[0];
+
+			var filePath = $"D:\\{modelName}_BeamReport.csv";
+			using (var writer = new CsvWriter(new StreamWriter(filePath)))
+			{
+				writer.WriteRecords(beamsReport);
+			}
+		}
+	}
+
+	public static class TeklaExtensions
+	{
+		public static List<ModelObject> ToList(this ModelObjectEnumerator enumerator)
+		{
+			var modelObjects = new List<ModelObject>();
+			while (enumerator.MoveNext())
+			{
+				var modelObject = enumerator.Current;
+				if(modelObject == null) continue;
+				modelObjects.Add(modelObject);
+			}
+
+			return modelObjects;
 		}
 	}
 }
